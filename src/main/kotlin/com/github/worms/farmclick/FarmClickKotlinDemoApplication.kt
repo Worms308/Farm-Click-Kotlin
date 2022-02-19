1f91e05d5b245d6d1d261aceeed35ffc54a35118
Patrycjusz Mania <worms308@wp.pl>
2022-02-19 14:27:14 +0100

    init commit

---
+++ src/main/kotlin/com/github/worms/farmclick/FarmClickKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.github.worms.farmclick
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FarmClickKotlinDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<FarmClickKotlinDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/worms/farmclick/FarmClickKotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.worms.farmclick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FarmClickKotlinDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
